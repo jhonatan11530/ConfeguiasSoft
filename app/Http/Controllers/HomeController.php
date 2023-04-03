d2e691d247badebfc73b4ec7625320c9ec6ab39f
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-04-02 21:33:05 -0500

    ok

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Registro;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 class HomeController extends Controller
 {
     function index()
@@ -15,7 +16,7 @@ class HomeController extends Controller
     {
         $Registro = Registro::find($id);
 
-        $pdf = \PDF::loadView('Dashboard.PlantillaPDF.plantilla',compact('Registro'));
+        $pdf = Pdf::loadView('Dashboard.PlantillaPDF.plantilla',compact('Registro'));
         return $pdf->stream();
     }
 }
